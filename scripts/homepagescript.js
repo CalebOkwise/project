81c0ae8fcdd3e4d613b937393b30e16a6ed10edb
CalebOkwise <79531682+CalebOkwise@users.noreply.github.com>
2022-08-17 16:32:53 +0100

    Add files via upload

--- scripts/homepagescript.js
+++ scripts/homepagescript.js
@@ -2,13 +2,31 @@ $(document).ready(function(){
 
     $row2form2 = $('.row2form2');
     $trigger = $('.row2trigger');
+    $btn = $('.herobtn');
+    $overlay = $('.overlay');
+    $popupcloser = $('.cancelbtn');
+    $herorow2 = $('.herorow2');
+
 
     /** */
     $trigger.click(function (){
 
         $row2form2.toggleClass('off');
+        $popupcloser.toggleClass('off');
         $trigger.toggleClass('off');
-        document.getElementById("animate").setAttribute("class", "herorow2 heightincrease");
+        $btn.toggleClass('off');
+        $overlay.toggleClass('off');
+        document.getElementById("animate").setAttribute("class", "herorow2 popupstyle");
+
+    })
+
+    $popupcloser.click(function (){
+        $row2form2.toggleClass('off');
+        $popupcloser.toggleClass('off');
+        $trigger.toggleClass('off');
+        $btn.toggleClass('off');
+        $overlay.toggleClass('off');
+        $herorow2.toggleClass('popupstyle');
 
     })
 
